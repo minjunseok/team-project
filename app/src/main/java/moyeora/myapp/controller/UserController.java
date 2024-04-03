0a49917f2b456b178213bb3e781a2fe6b3d46026
anhr-0819 <anhr0819@gmail.com>
2024-04-03 09:35:10 +0900

    .

--- app/src/main/java/moyeora/myapp/controller/UserController.java
+++ app/src/main/java/moyeora/myapp/controller/UserController.java
@@ -53,31 +53,9 @@ public class UserController implements InitializingBean {
   }
 
   @GetMapping("list")
-  public void list(
-//      @RequestParam(defaultValue = "1") int pageNo,
-//      @RequestParam(defaultValue = "3") int pageSize,
-      Model model) throws Exception {
+  public void list(Model model) throws Exception {
 
-//    if (pageSize < 3 || pageSize > 20) {
-//      pageSize = 3;
-//    }
-//
-//    if (pageNo < 1) {
-//      pageNo = 1;
-//    }
-//
-//    int numOfRecord = userService.countAll();
-//    int numOfPage = numOfRecord / pageSize + ((numOfRecord % pageSize) > 0 ? 1 : 0);
-//
-//    if (pageNo > numOfPage) {
-//      pageNo = numOfPage;
-//    }
-
-//    model.addAttribute("list", userService.list(pageNo, pageSize));
     model.addAttribute("list", userService.list());
-//    model.addAttribute("pageNo", pageNo);
-//    model.addAttribute("pageSize", pageSize);
-//    model.addAttribute("numOfPage", numOfPage);
   }
 
   @GetMapping("view")
