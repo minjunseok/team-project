e95b06f48636ec3bdd4f598f54a4f81dde49bbb3
Seung-Cheol <rlatmdcjf10@naver.com>
2024-04-05 16:54:58 +0900

    .

--- app/src/main/java/moyeora/myapp/vo/Post.java
+++ app/src/main/java/moyeora/myapp/vo/Post.java
@@ -2,12 +2,9 @@ package moyeora.myapp.vo;
 
 import java.util.Date;
 import java.util.List;
-<<<<<<< HEAD
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-=======
->>>>>>> 36279d08100cc172f09ed80ecfd45e8342e6d9fd
 
 import lombok.Data;
 
@@ -15,13 +12,8 @@ import lombok.Data;
 public class Post {
   private int no;
   private int schoolNo;
-  private User userNo;
-  private User User;
+  private int userNo;
   private int categoryNo;
-<<<<<<< HEAD
-=======
-  private List<AttachedFile> fileList;
->>>>>>> 36279d08100cc172f09ed80ecfd45e8342e6d9fd
   private String content;
   private Date createdAt;
   private int likeCount;
