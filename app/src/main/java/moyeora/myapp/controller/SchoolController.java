dc049093f1f1b37b1032b5ab06c798b014ddc689
sungchan98 <jvnhkdm0807@gmail.com>
2024-04-08 14:49:39 +0900

    ,

--- app/src/main/java/moyeora/myapp/controller/SchoolController.java
+++ app/src/main/java/moyeora/myapp/controller/SchoolController.java
@@ -8,6 +8,7 @@ import moyeora.myapp.service.TagService;
 import moyeora.myapp.vo.School;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,37 +26,37 @@ public class SchoolController {
   private final TagService tagService;
   private final SchoolService schoolService;
   private final PostService postService;
+  //private final StorageService storageService;
   final static Log log = LogFactory.getLog(SchoolController.class);
 
 
-
-  @GetMapping("form")
-  public void form(Model model, int schoolNo) {
-    model.addAttribute("postlists",postService.findBySchoolPostList(schoolNo));
-  }
-
   @PostMapping("add")
-  public void add(School school) {
+  public void add(School school, MultipartFile file) throws Exception{
+
     log.debug("============sdfsdsdf");
+    if(file.getSize() > 0) {
+    //  String filename = storageService
+    }
+
+
   }
 
   @GetMapping("view")
   public void view() {
 
   }
-//  @GetMapping("form")
-//  public void form(Model model) {
-//    model.addAttribute("tags", tagService.findAllTag());
-
-//  }
+  @GetMapping("form")
+  public void form(Model model) {
+    model.addAttribute("tags", tagService.findAllTag());
+  }
 
   @GetMapping("search")
   public void search(Model model,
       @RequestParam String name) {
     System.out.println(name);
     model.addAttribute("name", schoolService.findBySchoolName(name));
-
   }
+
 
 //  @GetMapping("postlist")
 //  public void findBySchoolPostList(Model model) {
