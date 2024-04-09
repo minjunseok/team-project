b93b026f5eab47e12b02ae90d513b0981e9ba1f0
Seung-Cheol <rlatmdcjf10@naver.com>
2024-04-09 17:38:03 +0900

    .

--- app/src/main/java/moyeora/myapp/service/UserService.java
+++ app/src/main/java/moyeora/myapp/service/UserService.java
@@ -8,6 +8,16 @@ public interface UserService {
 
     User get(int no);
 
+    List<User> list();
+
+    User get(String email);
+
+    String getEmail(String name, String phone);
+
+    void update(User user);
+
+    void delete(int no);
+
     List<String> findEmailByUserList(List<Integer> userList);
 
     void downGrade(List<Integer> userList);
