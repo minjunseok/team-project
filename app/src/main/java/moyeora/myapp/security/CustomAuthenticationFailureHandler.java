d062d8ae53217deeded93b6a271c30362040d117
anhr-0819 <anhr0819@gmail.com>
2024-04-12 18:02:10 +0900

    Authentication Handler 수정

--- app/src/main/java/moyeora/myapp/security/CustomAuthenticationFailureHandler.java
+++ app/src/main/java/moyeora/myapp/security/CustomAuthenticationFailureHandler.java
@@ -6,12 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @Component
