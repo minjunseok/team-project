78bee5c57cfa48773d18c0cf5df35b6185e1fad6
anhr-0819 <anhr0819@gmail.com>
2024-04-17 01:36:48 +0900

    테스트 페이지 추가

--- app/src/main/java/moyeora/myapp/service/ChatService.java
+++ app/src/main/java/moyeora/myapp/service/ChatService.java
@@ -1,11 +1,12 @@
 package moyeora.myapp.service;
 
-import moyeora.myapp.vo.ChatRoom;
+import moyeora.myapp.vo.ChatMessage;
+import org.springframework.web.socket.WebSocketSession;
 
 import java.util.List;
 
 public interface ChatService {
-    List<ChatRoom> list();
-    ChatRoom add(ChatRoom chatRoom);
 
+    List<ChatMessage> findChatMessageBySchoolNo(int schoolNo);
+    <T> void sendMessage(WebSocketSession session, T message);
 }
