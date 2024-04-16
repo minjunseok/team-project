bec405cb43b0613d6f9792bedeb3d8c815020b01
anhr-0819 <anhr0819@gmail.com>
2024-04-17 01:36:48 +0900

    테스트 페이지 추가

--- app/src/main/java/moyeora/myapp/dao/ChatDao.java
+++ app/src/main/java/moyeora/myapp/dao/ChatDao.java
@@ -1,7 +1,13 @@
 package moyeora.myapp.dao;
 
+import moyeora.myapp.vo.ChatMessage;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ChatDao {
+
+    void addChatMessage(ChatMessage chatMessage);
+    List<ChatMessage> findChatMessageByChatRoomNo(int roomNo);
 }
--- app/src/main/java/moyeora/myapp/vo/ChatMessage.java
+++ app/src/main/java/moyeora/myapp/vo/ChatMessage.java
@@ -1,7 +1,15 @@
 package moyeora.myapp.vo;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import moyeora.myapp.service.ChatService;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
 @Data
 public class ChatMessage {
     public enum MessageType {
@@ -9,7 +17,31 @@ public class ChatMessage {
     }
 
     private MessageType type;
-    private int roomNo; // 채팅방번호 ( dm_no, gm_no)
+
+    private int gm_no;
+    private int schoolNo;
     private int sender; // 메세지 보낸 사람
     private String message; // 메세지
+    private String photo;
+
+    private Set<WebSocketSession> sessions = new HashSet<>();
+
+    @Builder
+    public ChatMessage(int schoolNo, int sender, String message) {
+        this.schoolNo = schoolNo;
+        this.sender = sender;
+        this.message = message;
+    }
+
+    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
+        if ( chatMessage.getType().equals(ChatMessage.MessageType.ENTER) ) {
+            sessions.add(session);
+            chatMessage.setMessage(chatMessage.getSender() + "님이 입장하셨습니다.");
+        }
+        sendMessage(chatMessage, chatService);
+    }
+
+    public <T> void sendMessage(T message, ChatService chatService) {
+        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+    }
 }
