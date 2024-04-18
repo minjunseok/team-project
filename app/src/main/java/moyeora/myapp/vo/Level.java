583efce88c7871cb0aa7a8df2c8505961939c156
Seung-Cheol <rlatmdcjf10@naver.com>
2024-04-18 16:01:25 +0900

    .

--- app/src/main/java/moyeora/myapp/vo/Level.java
+++ app/src/main/java/moyeora/myapp/vo/Level.java
@@ -1,14 +1,5 @@
 package moyeora.myapp.vo;
 
-<<<<<<< HEAD
-
-import lombok.Data;
-
-@Data
-public class Level {
-  private int no;
-  private String name;
-=======
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +11,5 @@ public class Level {
 
   private String levelName;
   private int levelNo;
->>>>>>> 0878dd73705bf6680a0ac6d43a1e2698838943da
 
 }
