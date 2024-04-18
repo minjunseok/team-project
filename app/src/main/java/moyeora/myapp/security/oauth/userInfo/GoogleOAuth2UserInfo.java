0eea5dc51b8ad396b7637058c53778f0b158c78a
anhr-0819 <anhr0819@gmail.com>
2024-04-18 18:50:51 +0900

    /oauth

--- app/src/main/java/moyeora/myapp/security/oauth/userInfo/GoogleOAuth2UserInfo.java
+++ app/src/main/java/moyeora/myapp/security/oauth/userInfo/GoogleOAuth2UserInfo.java
@@ -1,10 +1,11 @@
 package moyeora.myapp.security.oauth.userInfo;
 
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
-public class GoogleOAuth2UserInfo implements OAuth2UserInfo{
+public class GoogleOAuth2UserInfo implements moyeora.myapp.security.oauth.userInfo.OAuth2UserInfo {
   private Map<String, Object> attributes;
 
   public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
