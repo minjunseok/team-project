c3bd8a48760eb6eb66e41160ae539b5755ec407d
mthej59 <mthej59@gamil.com>
2024-04-19 20:03:01 +0900

    이미지 이름 간소화 중

--- app/src/main/java/moyeora/myapp/dao/PostDao.java
+++ app/src/main/java/moyeora/myapp/dao/PostDao.java
@@ -10,6 +10,9 @@ public interface PostDao {
 
   void add(Post post);
 
+  // String으로 이미지 이름 강제 저장
+//  void add(String post);
+
   List<Post> findAll(@Param("categoryNo") int categoryNo);
 
   int delete(@Param("no") int no, @Param("schoolNo") int schoolNo);
