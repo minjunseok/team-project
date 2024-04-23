d629a2a49eb9006eaddd8f39bdf05fd478268f91
mthej59 <mthej59@gamil.com>
2024-04-23 16:59:16 +0900

    검색기능 하는중

--- app/src/main/java/moyeora/myapp/App.java
+++ app/src/main/java/moyeora/myapp/App.java
@@ -1,5 +1,8 @@
 package moyeora.myapp;
 
+import moyeora.myapp.service.UserService;
+import moyeora.myapp.vo.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -7,6 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -24,13 +29,19 @@ import org.springframework.web.bind.annotation.GetMapping;
         "classpath:config/email.properties"
 })
 public class App {
+
+
+  @Autowired //로그인 유저 번호 사용하기 위해서 적용
+  private UserService userService;
   public static void main(String[] args) throws Exception {
     System.out.println("과제관리 시스템 서버 실행!");
     SpringApplication.run(App.class, args);
   }
 
   @GetMapping("/home")
-  public void home() {
+  public void home(HttpSession session) {
+    User user = userService.get(1); // 세션으로 1번 유저의 정보를 받아서 사용
+    session.setAttribute("loginUser", user);
   }
 
   @GetMapping("/about")
