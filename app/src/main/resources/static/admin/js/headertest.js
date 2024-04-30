bb36479f15d62531c23902489dd6bff092a0d31c
mthej59 <mthej59@gamil.com>
2024-04-30 11:06:18 +0900

    모여라 헤더 완료

--- app/src/main/resources/static/admin/js/headertest.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.header_toggleBtn');
-const menu = document.querySelector('.header_menu');
-const icons = document.querySelector('.header_icons');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
