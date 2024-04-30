728a07b276049555d0631f6f11110d774fd14551
mthej59 <mthej59@gamil.com>
2024-04-30 20:52:13 +0900

    공통헤더 list.html에 적용

--- app/src/main/resources/static/admin/js/header.js
+++ app/src/main/resources/static/admin/js/header.js
@@ -1,8 +1,10 @@
-const toggleBtn = document.querySelector('.header_toggleBtn');
-const menu = document.querySelector('.header_menu');
-const icons = document.querySelector('.header_icons');
+window.onload = () => {
+  const toggleBtn = document.querySelector(".header_toggleBtn");
+  const menu = document.querySelector(".header_menu");
+  const icons = document.querySelector(".header_icons");
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
+  //  toggleBtn.addEventListener("click", () => {
+  //    menu.classList.toggle("active");
+  //    icons.classList.toggle("active");
+  //  });
+};
