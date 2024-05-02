1eb77333b4fa7525a02b9ba6d4d0507180059f47
Seung-Cheol <rlatmdcjf10@naver.com>
2024-05-02 19:50:45 +0900

    #19 log4j2 포기 kafka에 직접 로그 작성 예정, 데이터 정리가 되지 않으므로

--- app/src/main/java/moyeora/myapp/aspect/ControllerExecutionAspect.java
+++ app/src/main/java/moyeora/myapp/aspect/ControllerExecutionAspect.java
@@ -1,35 +1,66 @@
 package moyeora.myapp.aspect;
 
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @Aspect
 public class ControllerExecutionAspect {
 
-    private static final Logger logger = LoggerFactory.getLogger(ServiceExecutionAspect.class);
+    private static final Logger logger = LogManager.getLogger(ControllerExecutionAspect.class);
 
     @Pointcut("execution(* moyeora.myapp.controller.*.*(..))")
     public void controllerMethod() {
     }
 
-    @Before("controllerMethod()")
-    public void logControllerMethodCall(JoinPoint joinPoint) {
+    @AfterReturning(pointcut = "controllerMethod()", returning = "result")
+    public void logControllerMethodCall(JoinPoint joinPoint, Object result) throws Exception {
         String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getSignature().getClass().getName();
         Object[] args = joinPoint.getArgs();
-
-        StringBuilder logMessage = new StringBuilder();
-        logMessage.append("메소드 ").append(methodName).append("파라미터: ");
+        Map<String, Object> map = new HashMap<>();
+        ObjectMapper obj = new ObjectMapper();
+        obj.getFactory().configure(JsonGenerator.Feature.QUOTE_FIELD_NAMES, false);
+        map.put("class", className);
+        map.put("method", methodName);
+        List<Object> arr = new ArrayList<>();
         for (Object arg : args) {
-            logMessage.append(arg).append(", ");
+            arr.add(objectConvert(arg));
         }
+        map.put("args", arr);
+        map.put("result", objectConvert(result));
+        System.out.println("============>" + obj.writeValueAsString(map));
+        logger.info(map);
+    }
 
-        logger.info(logMessage.toString());
+    public Object objectConvert(Object arg) throws Exception {
+        if (arg instanceof Integer) {
+            return arg;
+        }
+        if (arg instanceof String) {
+            return arg;
+        }
+        if (arg instanceof List<?>) {
+            List<Object> arr = new ArrayList<>();
+            for (Object obj : (List) arg) {
+                arr.add(objectConvert(obj));
+            }
+        }
+        ObjectMapper obj = new ObjectMapper();
+        obj.getFactory().configure(JsonGenerator.Feature.QUOTE_FIELD_NAMES, false);
+        return obj.writeValueAsString(arg);
     }
 }
