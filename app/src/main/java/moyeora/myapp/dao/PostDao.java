53df2171081d65cee75c8749e3979f777f7a48a8
mthej59 <mthej59@gamil.com>
2024-05-04 22:57:23 +0900

    새 글 작성 modal 작업중

--- app/src/main/java/moyeora/myapp/dao/PostDao.java
+++ app/src/main/java/moyeora/myapp/dao/PostDao.java
@@ -1,50 +1,53 @@
 package moyeora.myapp.dao;
 
-import java.util.List;
 import moyeora.myapp.vo.Post;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 @Mapper
 public interface PostDao {
 
-  void add(Post post);
+    void add(Post post);
 
-  List<Post> findAll(@Param("categoryNo") int categoryNo);
+    List<Post> findAll(@Param("categoryNo") int categoryNo);
 
-  int delete(@Param("no") int no, @Param("schoolNo") int schoolNo);
+    int delete(@Param("no") int no, @Param("schoolNo") int schoolNo);
 
-  Post findBy(int no);
+    Post findBy(int no);
 
-  int update(Post post);
+    int update(Post post);
 
-  int countAll(int categoryNo);
+    int countAll(int categoryNo);
 
-  public List<Post> findByLike();
+    public List<Post> findByLike();
 
-  public List<Post> findByFollow();
+    public List<Post> findByFollow();
 
-  public List<Post> findByUser(int no);
+    public List<Post> findByUser(int no);
 
-  public List<Post> findByFollow(int no);
+    public List<Post> findByFollow(int no);
 
-  String findByPost(int schoolNo, String content);
+    String findByPost(int schoolNo, String content);
 
 
-  public List<Post> findBySchoolPost();
+    public List<Post> findBySchoolPost();
 
-  // 필터 내용으로 검색했을 때
-  List<Post> findBySchoolContent(int schoolNo, @Param("keyword") String keyword);
-  // 필터 작성자로 검색했을 때
-  List<Post> findBySchoolUserName(int schoolNo, @Param("keyword") String keyword);
+    // 필터 내용으로 검색했을 때
+    List<Post> findBySchoolContent(int schoolNo, @Param("keyword") String keyword);
 
-  List<Post> findBySchool(int schoolNo);
+    // 필터 작성자로 검색했을 때
+    List<Post> findBySchoolUserName(int schoolNo, @Param("keyword") String keyword);
 
-  List<Post> findBySchoolUserName(@Param("keyword") String keyword);
+    List<Post> findBySchool(int schoolNo);
+
+    List<Post> findBySchoolUserName(@Param("keyword") String keyword);
 //  Post findBy(int no);
 
-  Post findByPost(int no , int schoolNo);
+    Post findByPost(int no, int schoolNo);
 
 
-  List<Post> findBySchoolPostList(int schoolNo);
+    List<Post> findBySchoolPostList(int schoolNo);
+
 }
