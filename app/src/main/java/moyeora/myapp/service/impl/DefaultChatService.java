cd78653948c45068e817e65b5a7bc9e0766408b5
anhr-0819 <anhr0819@gmail.com>
2024-05-21 13:08:11 +0900

    서버용으로 설정 변경

--- app/src/main/java/moyeora/myapp/service/impl/DefaultChatService.java
+++ app/src/main/java/moyeora/myapp/service/impl/DefaultChatService.java
@@ -2,7 +2,6 @@ package moyeora.myapp.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import moyeora.myapp.dao.ChatDao;
-import moyeora.myapp.vo.Alert;
 import moyeora.myapp.vo.Dm;
 import moyeora.myapp.vo.DmRoom;
 import moyeora.myapp.vo.Gm;
@@ -21,14 +20,14 @@ public class DefaultChatService {
     @Transactional
     public void saveGm(Gm gm) throws Exception {
         chatDao.saveGm(gm);
-        Alert alert = new Alert();
+/*        Alert alert = new Alert();
         alert.setUserNo(gm.getSender().getNo()); // 알림 수신자 번호
         alert.setName("알림 타이틀"); // 알림 타이틀 문구
         alert.setContent("타이틀 밑에 영역에 넣을 상세 내용"); // 알림 내용 문구
         alert.setPhoto("알림 썸네일에 들어갈 사진 경로(NCD 사용 기준)"); // 사진 파일명
         alert.setType(1); // 알림 타입 1.좋아요 2.댓글 3.팔로잉
         alert.setRedirectPath("/클릭시 이동할 경로"); // 리다이렉트경로 지정
-        notificationService.add(alert);
+        notificationService.add(alert);*/
     }
 
     public Gm getGm(int no) {
