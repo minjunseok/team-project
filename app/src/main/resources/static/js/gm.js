96ed963cad58a75d30c2e7ddb91c925435e5880c
anhr-0819 <anhr0819@gmail.com>
2024-05-21 13:08:11 +0900

    서버용으로 설정 변경

--- app/src/main/resources/static/js/gm.js
+++ app/src/main/resources/static/js/gm.js
@@ -18,7 +18,7 @@
   }
 
   const stompClientGm = new StompJs.Client({
-      brokerURL: 'ws://localhost:8080/ws'
+      brokerURL: 'ws://175.45.194.120/ws'
   });
 
   stompClientGm.onConnect = (frame) => {
